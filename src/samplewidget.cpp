04213ee9cee4b25556794256c7aad2db6b1d57a6
Blagovest Petrov <blagovest@petrovs.info>
2026-03-12 23:49:12 +0200

    Fix segfault when a sample is edited
    Signed-off-by: Blagovest Petrov <blagovest@petrovs.info>

--- src/samplewidget.cpp
+++ src/samplewidget.cpp
@@ -382,7 +382,7 @@ void SampleWidget::slotView()
 	{
 		if(!layoutForPrint && !textLayoutFT->isLayoutFinished())
 		{
-			connect(textLayoutFT, SIGNAL(layoutFinished()), this, SLOT(slotView()));
+			connect(textLayoutFT, SIGNAL(layoutFinished()), this, SLOT(slotView()), Qt::UniqueConnection);
 			textLayoutFT->stopLayout();
 			qDebug()<<"\tLayout stopped";
 			layoutSwitch = false;
@@ -438,12 +438,21 @@ void SampleWidget::slotView()
 			if(!layoutForPrint)
 			{
 				layoutThread->setLayout(textLayout, list, fSize, f, hinting());
-				connect(textLayout, SIGNAL(drawPixmapForMe(int,double,double,double)), this, SLOT(drawPixmap(int,double,double,double)));
+				connect(textLayout, SIGNAL(drawPixmapForMe(int,double,double,double)), this, SLOT(drawPixmap(int,double,double,double)), Qt::UniqueConnection);
 //				connect(textLayoutFT, SIGNAL(drawBaselineForMe(double)), this, SLOT(drawBaseline(double)));
-				connect(textLayout, SIGNAL(layoutFinished()), this, SLOT(endLayout()));
+				connect(textLayout, SIGNAL(layoutFinished()), this, SLOT(endLayout()), Qt::UniqueConnection);
 				layoutSwitch = true;
 				pixmapDrawn = 0;
-				layoutThread->start();
+				if(layoutThread->isRunning())
+				{
+					// Thread hasn't fully exited yet (still cleaning up after previous run).
+					// QThread::start() silently does nothing on a running thread, so defer.
+					connect(layoutThread, SIGNAL(finished()), this, SLOT(slotView()), Qt::UniqueConnection);
+				}
+				else
+				{
+					layoutThread->start();
+				}
 			}
 			else
 			{
