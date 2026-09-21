896465ef1799d43b8f1d103005a00d2f9097a37a
Blagovest Petrov <blagovest@petrovs.info>
2026-09-21 14:41:38 +0300

    Bind SQL values instead of interpolating them

--- src/fmfontdb.h
+++ src/fmfontdb.h
@@ -32,6 +32,7 @@
 */
 
 class FontItem;
+class QSqlQuery;
 
 typedef QPair<FontItem*, QString> FontDBResult;
 
@@ -121,7 +122,6 @@ class FMFontDb : public QObject, public QSqlDatabase
 
 		QList<FontItem*> Fonts ( const QVariant& pattern, Field field );
 		QList<FontItem*> Fonts ( const QVariant& pattern, InfoItem info, int codeLang = 0 );
-		QList<FontItem*> Fonts ( const QString& whereString, Table table );
 		int FontCount();
 
 		QStringList getTags();
@@ -150,13 +150,16 @@ class FMFontDb : public QObject, public QSqlDatabase
 		// ensure tables are created
 		void initFMDb();
 
+		// whereString carries one '?' per entry of values
+		QList<FontItem*> Fonts ( const QString& whereString, const QVariantList& values, Table table );
+		bool execBound ( QSqlQuery& query, const QString& statement, const QVariantList& values = QVariantList() );
+
 		QStringList priorList;
 		QMap<Field, QString> fieldName;
 		QMap<Table, QString> tableName;
 		QMap<QString,int> cacheId;
 		QMap<int, QString> reverseCacheId;
 
-		QString getIdStringFast;
 		int getId ( const QString& fontid );
 		int internalCounter;
 
